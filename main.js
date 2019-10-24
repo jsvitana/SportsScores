669754f3892c47f64e7dff6ffb419af01a266622
Jonathan <jsvitana@yahoo.com>
2019-10-24 08:19:37 -0400

    npm

--- main.js
+++ main.js
@@ -5,6 +5,6 @@ let game = toString(2019102400)
 fetch("http://www.nfl.com/liveupdate/scores/scores.json")
 .then((resp) => resp.json()
 .then((data) => {
-    score = data;
+    score = data.toString(2019102400).home.abbr;
     alert(score);
     }))
