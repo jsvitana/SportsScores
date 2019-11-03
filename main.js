d97801796501dfec65a62a5b2b4285cc1505c0c5
jsvitana <51210786+jsvitana@users.noreply.github.com>
2019-11-02 23:04:21 -0400

    Update

--- main.js
+++ main.js
@@ -1,16 +1,24 @@
-let score = "";
+let homeTeam = "";
+let homeScore = "";
+let awayTeam = "";
+let awayScore = "";
 //const url = "http://www.nfl.com/liveupdate/game-center/2019102100/2019102100_gtd.json"
 
 const app = new Vue({
     el: '#test',
     data: {
-        message: 'this'
+        score: ''
     },
     created () {
         fetch("http://www.nfl.com/liveupdate/scores/scores.json")
         .then((resp) => resp.json()
         .then((data) => {
-            this.message = data[Object.keys(data)[0]].home.abbr;
+            let chosenGame = data[Object.keys(data)[0]];
+            //homeTeam = 
+            homeScore = chosenGame.home.score.T;
+            //awayTeam = 
+            awayScore = chosenGame.away.score.T;
+            this.score = homeScore + "-" + awayScore;
             }))
     }
 })
